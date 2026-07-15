c3e943036bf42b525f4f29a24cad1cc73e1755f7
josesilva <franciscojoseduartesilva@gmail.com>
2026-07-15 11:28:31 -0300

    Aula11

--- src/Aula10/Alunoview.java
+++ src/Aula10/Alunoview.java
@@ -2,7 +2,7 @@ package Aula10;
 
 public class Alunoview {
 public static void main(String[] args) {
-    Aluno aluno = new Aluno("Julio", "Programador de sistemas", 6, 10);
+    Aluno aluno = new Aluno("Julio", "Programador de sistemas", 6.5, 8.5);
 
     System.out.println("Nome:" + aluno.getNome());
     System.out.println("Curso:" + aluno.getCurso());
--- src/Aula10/Produtoview.java
+++ src/Aula10/Produtoview.java
@@ -2,7 +2,7 @@ package Aula10;
 
 public class Produtoview {
 public static void main(String[] args) {
-    Produto produto = new Produto("Notebook", 3500.00, 10);
+    Produto produto = new Produto("Smartphone", 2500.00, 10);
 
     System.out.println("Nome: " + produto.getNome());
     System.out.println("Preço: R$ " + produto.getPreco());
